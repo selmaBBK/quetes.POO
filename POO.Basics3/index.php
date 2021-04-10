048d99c6a92b8816615410b082c13ead8b392104
selmaBBK <77289543+selmaBBK@users.noreply.github.com>
2021-04-10 18:27:50 +0200

    Create index.php

---
+++ POO.Basics3/index.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'Vehicle.php';
+require_once 'Bicycle.php';
+require_once 'Truck.php';
+require_once 'Car.php';
+require_once 'HighWay.php';
+require_once 'MotorWay.php';
+require_once 'PedestrianWay.php';
+require_once 'ResidentialWay.php';
+
+$motorWay = new MotorWay();
+$pedestrianWay = new PedestrianWay();
+$residentialWay = new ResidentialWay();
